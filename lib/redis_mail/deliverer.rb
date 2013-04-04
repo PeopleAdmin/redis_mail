7531a0d8d6aafa34b4846d88c122c3f798476a76
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-04-04 18:50:22 -0500

    Catch messages sent by Mail gem and store them in redis
    - use a redis set to keep track of all mailboxes (recipients)
    - each mailbox is a redis list named after the recipient

--- lib/redis_mail/deliverer.rb
+++ lib/redis_mail/deliverer.rb
@@ -1,3 +1,4 @@
+require 'redis-namespace'
 module RedisMail
   class Deliverer
     attr :settings
@@ -6,9 +7,14 @@ module RedisMail
       @settings = values
     end
 
+    def redis
+      @redis ||= Redis::Namespace.new(:redis_mail, redis: settings[:redis])
+    end
+
     def deliver!(mail)
       mail.destinations.uniq.each do |to|
-        puts "sending mail to #{to}"
+        redis.sadd :mailboxes, to
+        redis.rpush "mailbox:#{to}", mail.to_s
       end
     end
   end
--- spec/deliverer_spec.rb
+++ spec/deliverer_spec.rb
@@ -1,11 +1,75 @@
 require 'mail'
+require 'redis'
 require 'redis_mail'
 
 describe RedisMail::Deliverer do
-  it "delivers the message to each recipient" do
-    mail = Mail.new to: "someone@example.com", from: "author@example.com",
-      subject: "Hello world", body: "This is the test email"
-    mail.delivery_method(RedisMail::Deliverer, {})
-    mail.deliver!
+  let(:redis) { Redis.new.tap {|r| r.select 15} }
+
+  before { redis.flushdb }
+
+  context "delivering a message" do
+    let(:sent_message) {
+      Mail.new {
+        to "someone@example.com"
+        to << "another@example.com"
+        from "author@example.com"
+        subject "Hello world"
+        body "This is the test email"
+        cc "interested@example.com"
+      }
+    }
+
+    before do
+      sent_message.delivery_method(RedisMail::Deliverer, {redis: redis})
+      sent_message.deliver!
+    end
+
+    it "creates a mailbox for tracking messages to each recipient" do
+      mailboxes = redis.smembers "redis_mail:mailboxes"
+      mailboxes.should have(3).items
+      mailboxes.should include "someone@example.com"
+      mailboxes.should include "another@example.com"
+      mailboxes.should include "interested@example.com"
+    end
+
+    it "stores the entire message for each recipient" do
+      sent = redis.lpop "redis_mail:mailbox:someone@example.com"
+      sent.should_not be_nil
+      sent.should == sent_message.to_s
+
+      sent = redis.lpop "redis_mail:mailbox:another@example.com"
+      sent.should_not be_nil
+      sent.should == sent_message.to_s
+
+      sent = redis.lpop "redis_mail:mailbox:interested@example.com"
+      sent.should_not be_nil
+      sent.should == sent_message.to_s
+    end
+
+    context "delivering multiple message to same recipient" do
+      let(:second_message) { Mail.new to: "someone@example.com", subject: "second"  }
+      let(:third_message) { Mail.new to: "someone@example.com", subject: "third"  }
+
+      before do
+        [second_message, third_message].each do |message|
+          message.delivery_method(RedisMail::Deliverer, {redis: redis})
+          message.deliver!
+        end
+      end
+
+      it "stores the messages in chronological order" do
+        sent = redis.lpop "redis_mail:mailbox:someone@example.com"
+        Mail.new(sent).should == sent_message
+
+        sent = redis.lpop "redis_mail:mailbox:someone@example.com"
+        Mail.new(sent).should == second_message
+
+        sent = redis.lpop "redis_mail:mailbox:someone@example.com"
+        Mail.new(sent).should == third_message
+
+        sent = redis.lpop "redis_mail:mailbox:someone@example.com"
+        sent.should be_nil
+      end
+    end
   end
 end
